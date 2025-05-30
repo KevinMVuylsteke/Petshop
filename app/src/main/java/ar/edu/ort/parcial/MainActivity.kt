275e9cbf931d5fdf96bc1518089895860d359f33
KevinMVuylsteke <kevinmvuylsteke@gmail.com>
2025-05-29 23:27:12 -0300

    Guardando cambios antes de cambiar a main

--- app/src/main/java/ar/edu/ort/parcial/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/parcial/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import ar.edu.ort.parcial.ui.theme.ParcialTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
